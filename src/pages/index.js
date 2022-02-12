bb5c84ef72ecc4c6661e15b357e88f4b9a175c64
Mark AI <chanthavong_12@hotmail.com>
2022-02-12 20:28:37 +0700

    init

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Head from '../components/head';
+
+import { Button } from '@nextui-org/react';
+
+
+export default () => (
+  <div>
+    <h2>Hello world</h2>
+    <Button>Default</Button>
+  </div>
+);
