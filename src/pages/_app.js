c22ff541102823e61ecef0d921638da3b8b9c982
Mark AI <chanthavong_12@hotmail.com>
2022-02-13 15:26:23 +0700

    add multi langauge

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,26 @@
+import { NextIntlProvider } from 'next-intl';
+import NextApp from 'next/app';
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react';
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, messages, pageProps }) => {
   return (
     // 2. Use at the root of your app
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextIntlProvider messages={messages}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextIntlProvider>
   );
-}
+};
+
+MyApp.getInitialProps = async function getInitialProps(context) {
+  const { locale } = context.router;
+  const messages = locale ? require(`../locales/${locale}.json`) : undefined;
+  return {
+    ...(await NextApp.getInitialProps(context)),
+    messages,
+  };
+};
 
 export default MyApp;
