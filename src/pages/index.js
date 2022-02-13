31178c50a77724a7e329e6843decef75a5c931da
Mark AI <chanthavong_12@hotmail.com>
2022-02-13 15:26:13 +0700

    add multi langauge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,37 @@
 import Link from 'next/link';
 import Head from '../components/head';
+import { useTranslations } from 'next-intl';
+import {
+  Grid,
+  Spacer,
+  Container,
+  Button,
+  Input,
+  Text,
+  Col,
+  Row,
+} from '@nextui-org/react';
 
-import { Button } from '@nextui-org/react';
+export default function () {
+  const h = useTranslations('Home');
+  const t = useTranslations('label');
 
-
-export default () => (
-  <div>
-    <h2>Hello world</h2>
-    <Button>Default</Button>
-  </div>
-);
+  return (
+    <Container align="center">
+      <h2>{h('hello')}</h2>
+      <Input placeholder={t('email')} id="email" />
+      <Spacer y={0.5} />
+      <Input.Password placeholder={t('password')} id="password" />
+      <Spacer y={0.5} />
+      <Button>{t('login')}</Button>
+      <Spacer y={1} />
+      <Link href="/lo" locale={false} >
+        <a>ພາສາລາວ</a>
+      </Link>
+      &nbsp; &nbsp;
+      <Link href="/en" locale={false}>
+        <a>English</a>
+      </Link>
+    </Container>
+  );
+}
